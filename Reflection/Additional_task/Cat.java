d3f03c1083703fc94e91b4f8967f2ef0252e7ac4
yevheniiholub <36192700+yevheniiholub@users.noreply.github.com>
2022-01-05 14:00:27 +0200

    Now cat inherited from Animal, and reflection done at Main

--- Reflection/Additional_task/Cat.java
+++ Reflection/Additional_task/Cat.java
@@ -1,25 +1,8 @@
 package Reflection.Additional_task;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Member;
+public class Cat extends Animal{
 
-public class Cat {
-    public void changeAnimal(Animal animal){
-        Class classVar = animal.getClass();
-        Field [] fields = classVar.getDeclaredFields();
-
-        try {
-            fields[0].setAccessible(true);
-            fields[0].set(animal, 123);
-
-            fields[1].setAccessible(true);
-            fields[1].set(animal, "Barney");
-
-            fields[2].setAccessible(true);
-            fields[2].set(animal, true);
-        }
-        catch (IllegalArgumentException | IllegalAccessException e) {
-            System.out.println(e.getMessage());
-        }
+    Cat (int age, String name, boolean hungry){
+        super(age, name, hungry);
     }
 }
--- Reflection/Additional_task/Main.java
+++ Reflection/Additional_task/Main.java
@@ -8,21 +8,43 @@ package Reflection.Additional_task;
 
  */
 
+import java.lang.reflect.Field;
+
 public class Main {
+
+    public static void changeAnimal(Cat cat) throws IllegalArgumentException, IllegalAccessException {
+        Class classVar = cat.getClass().getSuperclass();
+        Field[] fields = classVar.getDeclaredFields();
+        for (Field field : fields) {
+            if (field.getType().equals(int.class)) {
+                field.setAccessible(true);
+                field.set(cat, 123);
+            } else if (field.getType().equals(String.class)) {
+                field.setAccessible(true);
+                field.set(cat, "Barney");
+            } else if (field.getType().equals(boolean.class)) {
+                field.setAccessible(true);
+                field.set(cat, true);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Animal animal = new Animal(14, "Bobby", false);
-        Cat cat = new Cat();
+        Cat cat = new Cat(14, "Bobby", false);
 
         System.out.println("\u001B[34m" + "Before changes: " + "\u001B[0m");
-        System.out.println("Age :" + animal.getAge());
-        System.out.println("Name :" + animal.getName());
-        System.out.println("Hungry :" + animal.isHungry());
-
-        cat.changeAnimal(animal);
+        System.out.println("Age :" + cat.getAge());
+        System.out.println("Name :" + cat.getName());
+        System.out.println("Hungry :" + cat.isHungry());
+        try {
+            changeAnimal(cat);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            System.out.println(e.getMessage());
+        }
 
         System.out.println("\u001B[34m" + "After changes: " + "\u001B[0m");
-        System.out.println("Age :" + animal.getAge());
-        System.out.println("Name :" + animal.getName());
-        System.out.println("Hungry :" + animal.isHungry());
+        System.out.println("Age :" + cat.getAge());
+        System.out.println("Name :" + cat.getName());
+        System.out.println("Hungry :" + cat.isHungry());
     }
 }
